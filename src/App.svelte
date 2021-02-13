80f7eb44d3ceef60e6060f14ef06d320624ea68b
Noah Syrkis <noah@syrkis.com>
2021-02-13 19:51:17 +0100

    back to ts

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
-<script>
+<script lang="ts">
         import Header from './comps/Header.svelte';
         import Footer from './comps/Footer.svelte';
         import Pentagon from './comps/Pentagon.svelte';
-        export let name;
+        export let name: string;
 </script>
 
 <main>
