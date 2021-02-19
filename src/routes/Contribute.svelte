e557a727aec58fa1fad68c0a65c9af2b6cd7f961
Noah Syrkis <noah@syrkis.com>
2021-02-19 18:41:32 +0100

    Factor deploy

--- src/routes/Contribute.svelte
+++ src/routes/Contribute.svelte
@@ -1,9 +1,8 @@
 <script lang='ts'>
-    import Form from '../comps/Form.svelte';
+    import Factor from '../comps/Factor.svelte';
 </script>
 <p>
 You can contribute to the Virian Project by donation or by data generation: One of our current areas of research is cultural dimensionlity. This means answering the question, "In what ways do cultures differ from one another?" Filling in an adjective completeing the sentence below contributes to the generation of a dataset that can help in answering this, allowing us to perform <a href='https://en.wikipedia.org/wiki/Factor_analysis'><i><b>Factor Analysis</b></i></a>.
 </p>
 
-<Form />
-
+<Factor />
