7db34530ee0327102a69180ebcfdfa43dc2a5832
Noah Syrkis <noah@syrkis.com>
2021-04-08 15:08:47 +0200

    update assist text

--- src/comps/Contactor.svelte
+++ src/comps/Contactor.svelte
@@ -5,7 +5,7 @@
 <section>
     <form autocomplete='off'>
     <p>
-    Another way to help is by evaluating weeks on the dimenions yilded by the process outlined above. This will allow us to find correlates between global web nehavior and the particular prevalence of that dimenions in society at a given point in time. If you want to help with this please tell us your 
+    Another way to help is by ranking weeks in society, as you experience them, on the dimenions yilded by the process outlined above. This will allow us to find correlates between global web behavior and these dimensions. To help please tell us your 
         <input type='text' placeholder='name' id='name'>
     and
         <input type='text' placeholder='e-mail address' id='email'>
--- src/routes/Assist.svelte
+++ src/routes/Assist.svelte
@@ -3,10 +3,11 @@
     import Contactor from '../comps/Contactor.svelte';
 </script>
 <p>
-    You can contribute to the Virian Project by <b><i>donation</i></b> or by data generation. Inspired by Haidth et al.'s <b><i><a href="https://en.wikipedia.org/wiki/Moral_foundations_theory" target='_blank'>Moral foundations theory</a></i></b>, we are currently trying to determine the dimensions on which cultures vary from one another. You can help with this by completing the sentence below. This will allow us to perform <a href='https://en.wikipedia.org/wiki/Factor_analysis' target='_blank'><i><b>Factor Analysis</b></i></a>.
+    You can assist The Virian Project by <b><i>donation</i></b> or data generation. Currently we are researching the ways in which cultures vary form one another—inspired by Haidth et al.'s <b><i><a href="https://en.wikipedia.org/wiki/Moral_foundations_theory" target='_blank'>MFT</a></i></b>—by performing <a href='https://en.wikipedia.org/wiki/Factor_analysis' target='_blank'><i><b>Factor Analysis</b></i></a> on our growing association data set. You can add to this by completing the sentence below.
+    
+
 </p>
 
-<br/>
 <br/>
 <Associator />
 <br/>
