c97f78fcaa14e2435350cbf64aa2fd438d4254ae
Noah Syrkis <noah@syrkis.com>
2021-04-08 15:13:20 +0200

    update assist contact form for mobile

--- src/comps/Contactor.svelte
+++ src/comps/Contactor.svelte
@@ -5,7 +5,7 @@
 <section>
     <form autocomplete='off'>
     <p>
-    Another way to help is by ranking weeks in society, as you experience them, on the dimenions yilded by the process outlined above. This will allow us to find correlates between global web behavior and these dimensions. To help please tell us your 
+    Another way to help is by ranking weeks in society, as you experience them, on the dimenions from the process above. This will allow us to find correlates between global web behavior and these dimensions. To help tell us your
         <input type='text' placeholder='name' id='name'>
     and
         <input type='text' placeholder='e-mail address' id='email'>
