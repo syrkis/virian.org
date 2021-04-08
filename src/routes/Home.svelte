7350300a0f1c0ce00887595878c9b2431e39f197
Noah Syrkis <noah@syrkis.com>
2021-04-08 17:43:16 +0200

    notification test and typos

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -4,7 +4,7 @@
 
 <main>
     <p>
-        Virian is a non-profit attempt in making the behaviour our cultural focus—as gagued through global web activity—explicit enough so as to be includeable in autonumous  media curation systems. The web, increasingly, is the nervous system of our world, and we need it to reflect, not our vices, but our values.
+        Virian is a non-profit attempt at making the behaviour our cultural focus—as gagued through global web activity—explicit enough so as to be includable in autonumous  media curation systems. The web, increasingly, is the nervous system of our world, and we need it to reflect, not our vices, but our values.
     </p>
     <br/>
     <br/>
