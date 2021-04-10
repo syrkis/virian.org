7e138acffc30529b22b3dfcb839bc2c5019ef921
Noah Syrkis <noah@syrkis.com>
2021-04-10 16:42:40 +0200

    add adjust plug text shorter

--- src/comps/Plug.svelte
+++ src/comps/Plug.svelte
@@ -4,7 +4,7 @@
 
 <main>
     <p>
-    The first iteation of <code>plug</code> will be a browser extension released late summer 2021. Initially it will evaluate the contents of your web activity on the cultural dimensions uncovered through <code><a href='assist'>assist/</a></code>. Exploring this evaluation should be viewed as a meditation of sorts. All analysis is done on device.
+    The first iteation of <code>plug</code> will be a browser extension released late summer 2021. Initially it will evaluate the contents of your web activity on the cultural dimensions uncovered through <code><a href='assist'>assist/</a></code>. Exploring this evaluation should be viewed as a meditation of sorts.
     </p>
 </main>
 
