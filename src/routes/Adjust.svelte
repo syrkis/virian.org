e53c83117b341f99f439f35e918f51c22cee874e
Noah Syrkis <noah@syrkis.com>
2021-04-10 16:40:20 +0200

    add adjust data/plug hide show behavior

--- src/routes/Adjust.svelte
+++ src/routes/Adjust.svelte
@@ -2,25 +2,52 @@
     import Data from '../comps/Data.svelte';
     import Plug from '../comps/Plug.svelte';
 
-    $: showData = null;
+    $: showSection = null;
+    $: showData = false;
+    $: showPlug = false;
 
     function toggleData() {
-        if (!showData) {
+        if (!showSection) {
+            $: showSection = true;
+            let dataCss = document.getElementById('data1')
+            dataCss.style.borderBottom = '2px solid #ccc'; 
             $: showData = true;
+        } else if (!showData) {
+            $: showData = true;
+            $: showPlug = false;
             let dataCss = document.getElementById('data1')
             let plugCss = document.getElementById('plug1')
             dataCss.style.borderBottom = '2px solid #ccc'; 
             plugCss.style.borderBottom = 'none';
-        }  
+        } else {
+            $: showSection = false;
+            let dataCss = document.getElementById('data1')
+            dataCss.style.borderBottom = 'none';  
+            $: showPlug = false;
+            $: showData = false;
+        }
     }
 
     function togglePlug() {
-        if (showData) {
+        if (!showSection) {
+            $: showSection = true;
+            let plugCss = document.getElementById('plug1')
+            plugCss.style.borderBottom = '2px solid #ccc'; 
+            $: showPlug = true;
+        }
+        else if (!showPlug) {
+            $: showPlug = true;
             $: showData = false;
             let dataCss = document.getElementById('data1')
             let plugCss = document.getElementById('plug1')
             plugCss.style.borderBottom = '2px solid #ccc'; 
             dataCss.style.borderBottom = 'none';
+        } else { 
+            $: showSection = false;
+            let plugCss = document.getElementById('plug1')
+            plugCss.style.borderBottom = 'none';  
+            $: showPlug = false;
+            $: showData = false;
         }
     }
 </script>
@@ -36,15 +63,16 @@
         <li id='plug'><code><button on:click={togglePlug}><span id='plug1'>plug</span></button></code></li>
     </ul>
     <br/>
-    {#if showData}
-        <Data />
-    {:else}
-        <Plug />
+    {#if showSection}
+        {#if showData}
+            <Data />
+        {:else}
+            <Plug />
+        {/if}
     {/if}
 </main>
 <style>
     #data { padding: 0 70px 0 0; }  
-
     #plug { padding: 0 0 0 70px; }
 
     button, button:active {
