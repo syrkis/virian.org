fb1efa7924e302466e0a0fe7d9d7352bf0fdbae4
Noah Syrkis <noah@syrkis.com>
2021-07-22 14:26:18 +0200

    center p (justify later)

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,12 +1,20 @@
 <script lang='ts'>
     import Grapher from '../comps/Grapher.svelte'; 
-</script>
+    import { onMount } from 'svelte';
 
+    
+</script>
 <main>
-    <p>
-    The <i>Virian Project</i> is an attempt at making the movment of our collective attention explicit enough to be feature in <code>AI</code>-based content curation systems. The web, increasingly, is our shared nervous system—we need it to guide our attention in a way that aligns with our values.
+    <div>
+    <p id='lorem'>
+    The <i>Virian Project</i> is an attempt at making the movments of our collective attention explicit enough to be a feature in <code>AI</code>-based content curation systems. The web, increasingly, is our shared nervous system—we need it to guide our attention in a way that aligns with our values.
     </p>
+</div>
     <br/>
     <br/>
     <Grapher />
 </main>
+
+<style>
+
+</style>
