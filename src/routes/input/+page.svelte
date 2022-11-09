1d440e97d80a3c32bb398ed3e312177a5f81b8d2
Noah Syrkis <noah@syrkis.com>
2022-11-09 21:33:38 +0100

    text updates

--- src/routes/input/+page.svelte
+++ src/routes/input/+page.svelte
@@ -5,7 +5,7 @@
     <div class="text">
         <p>
             For now, to support Virian, subscribe to our <i><a href="https://virian.substack.com">quarterly report</a></i>,
-            or spread the word. We will let you know soon when there's more to do.
+            or spread the word. We will let you know when there's more to do.
         </p>
     </div>
 </main>
--- src/routes/output/+page.svelte
+++ src/routes/output/+page.svelte
@@ -5,7 +5,7 @@
     <div class="text">
         <p>
             Subscribe to the <i><a href="https://virian.substack.com">Virian Report</a></i> for quarterly
-            quantitative global cultural analyses, and general updates about the Virian Proect.
+            quantitative cultural analyses, and general updates about the Virian Project.
         </p>
     </div>
 </main>
