ef76a5d6c2245d618a457ed21c230b7378da5cac
Noah Syrkis <noah@syrkis.com>
2022-11-09 22:21:04 +0100

    text updates

--- src/routes/input/+page.svelte
+++ src/routes/input/+page.svelte
@@ -2,7 +2,7 @@
     <div class="text">
         <p>
             For now, to support Virian subscribe to our<i><a href="https://virian.substack.com">report</a></i>or
-            spread the word. We'll let you know when there's more to do.
+            spread the word. We will let you know when there is more to do.
         </p>
     </div>
 </main>
