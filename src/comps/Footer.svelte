b0c4d063f8efb57c80961f46c22a86137c677946
Noah Syrkis <noah@syrkis.com>
2022-11-12 18:04:10 +0100

    begin adding path

--- src/comps/Footer.svelte
+++ src/comps/Footer.svelte
@@ -29,7 +29,7 @@
         color: #fff;
         text-align: center;
         line-height: 50px;
-        padding: 3em 0;
+        padding: 6em 0;
     }
     .fab {
         padding: 0 10px;
--- src/comps/Pentaglyph/Pentaglyph.svelte
+++ src/comps/Pentaglyph/Pentaglyph.svelte
@@ -13,13 +13,12 @@
             <svg width="100%" height="100%">
                 <g style="transform: translate(50%, 50%)">
                     {#each Object.keys(data.data.fr[8]) as dim, i}
-                        <g transform="rotate({15 - i * 360 / Object.keys(data.data.fr[8]).length})">
-                            <text style='letter-spacing: 0.2em' class='dim' fill="white" text-anchor="middle" dominant-baseline="middle">
-                                {dim.toUpperCase()}
+                        <g transform="rotate({i * 360 / 5 + 18}) translate(300)">
+                            <text transform="rotate({-((- i * 360) / 5 + 18)}) translate(-10)"
+                                  class='dim' fill="white" text-anchor="middle" dominant-baseline="middle">
+                                {dim.toLowerCase()}
                             </text>
-                            <circle r="10" fill="white" transform="translate(40, 40)"></circle>
-                            <circle r="20" fill="white" transform="translate(120, 120)"></circle>
-                            <circle r="5" fill="white" transform="translate(180, 180)"></circle>
+                            <path d="M 0 0 L -40 0" transform="translate({i * 60})" stroke="white" stroke-width="10" fill="none" />
                         </g>
                     {/each}
                 </g>
@@ -32,7 +31,7 @@
 <style>
 
     .dim {
-        transform: translate(25%);
+        letter-spacing: 0.2em;
     }
 
     .container {
