4d7fe135b67c0c94ac769f49229278b0c5924e85
Noah Syrkis <noah@syrkis.om>
2022-11-23 06:29:14 -0500

    shorten output text

--- src/routes/output/+page.svelte
+++ src/routes/output/+page.svelte
@@ -1,7 +1,7 @@
 <main>
     <div class="text">
         <p>
-            We publish a quarterly quantitative cultural analysis <i><a href="https://virian.substack.com">report</a></i>,
+            We publish a quantitative cultural analysis <i><a href="https://virian.substack.com">report</a></i>,
             and provide advanced machine learning consulting services.
         </p>
     </div>
