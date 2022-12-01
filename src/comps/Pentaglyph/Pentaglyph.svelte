c1fedd41ab269b56b4ade533f0b67375336819dc
Noah Syrkis <noah@syrkis.com>
2022-12-01 17:15:53 +0100

    uipdate stuff

--- src/comps/Pentaglyph/Pentaglyph.svelte
+++ src/comps/Pentaglyph/Pentaglyph.svelte
@@ -99,9 +99,7 @@
         <p>
             *<a href="https://ess-search.nsd.no/"><i>European Social Survey</i></a> based subspace of
             <a href="https://en.wikipedia.org/wiki/Theory_of_Basic_Human_Values" rel="noreferrer" target="_blank">
-                <i>Schwartz values</i></a>
-            <br>
-            (length encodes mean, and spread encodes variance):
+                <i>Schwartz values</i></a>. Length encodes mean, and spread encodes variance.
         </p>
     </div>
 </div>
