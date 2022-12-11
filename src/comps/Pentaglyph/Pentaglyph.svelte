34fdb889141f6148adbd61acc16637d18b3ba25e
Noah Syrkis <noah@syrkis.com>
2022-12-11 15:09:55 -0300

    Update Pentaglyph.svelte

--- src/comps/Pentaglyph/Pentaglyph.svelte
+++ src/comps/Pentaglyph/Pentaglyph.svelte
@@ -153,10 +153,10 @@
     </div>
     <div class="text">
         <p>
-            *<a href="https://ess-search.nsd.no/"><i>European Social Survey</i></a> derived
+            *<a href="https://ess-search.nsd.no/"><i>European Social Survey</i></a> based
             <a href="https://en.wikipedia.org/wiki/Theory_of_Basic_Human_Values" rel="noreferrer" target="_blank">
                 <i>Schwartz values</i></a>. Length/spread encodes mean/variance
-            (drag to explore time/place).
+            Drag to explore time/place.
         </p>
     </div>
 </div>
