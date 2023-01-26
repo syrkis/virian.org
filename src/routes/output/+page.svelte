86d257e8b45d670736f0ebea439e79f1e74a2a78
Noah Syrkis <noah@syrkis.com>
2023-01-26 08:54:30 -0300

    update output text to be sleezy consultant like

--- src/routes/output/+page.svelte
+++ src/routes/output/+page.svelte
@@ -2,7 +2,13 @@
     <div class="text">
         <p>
             We publish the quaterly <i><a href="https://virian.substack.com">Virian Report,</a></i>
-            and provide machine learning- and poltical network analysis<i><a href="mailto:desk@virian.org?subject=Machine learning consulting">services and advice.</a></i>
+            and provide
+			<i><a href="mailto:desk@virian.org?subject=Machine learning inquiry">
+				machine learning</a></i> and
+			<i><a href="mailto:desk@virian.org?subject=Machine learning consulting">
+				poltical network analysis</a></i>
+			advice and services.
+
         </p>
     </div>
 </main>
