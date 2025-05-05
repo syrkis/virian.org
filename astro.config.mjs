182b2b9f316a7be76cbfacc885ed3aa506921b25
Noah Syrkis <noah@syrkis.com>
2025-05-05 12:48:27 +0200

    refector

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import { typst } from "astro-typst";
-import sitemap from "@astrojs/sitemap";
+
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://virian.com",
+  integrations: [typst(), sitemap()],
   adapter: netlify(),
-  integrations: [sitemap(), typst()],
-  vite: {
-    server: {
-      watch: {
-        // Add .typ files to the watch list
-        ignored: ["!**/*.typ"],
-        // Ensure high poll interval for more reliable watching
-        usePolling: true,
-        interval: 1000,
-      },
-    },
-  },
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,23 +1,23 @@
 import { defineCollection, z } from "astro:content";
-import { file, glob } from "astro/loaders"; // Not available with legacy API
+import { glob } from "astro/loaders"; // Not available with legacy API
 
 // Extend the schema to ensure type is always either "text" or "code"
-const validTypes = ["text", "code"] as const;
+// const validTypes = ["text", "code"] as const;
 
 const typCollection = defineCollection({
-  schema: z.object({
-    slug: z.string(),
-    title: z.string(),
-    author: z.string().optional().default("Noah Syrkis"),
-    desc: z.any().optional(),
-    cover: z.string(),
-    published: z.boolean().optional().default(false),
-    date: z.any(),
-    type: z.string().optional(),
-  }),
-  loader: glob({ pattern: "**/*.typ", base: "./src/posts" }),
+    loader: glob({ pattern: "*.typ", base: "./src/posts" }),
+    schema: z.object({
+        slug: z.string(),
+        title: z.string(),
+        author: z.string().optional().default("Noah Syrkis"),
+        desc: z.any().optional(),
+        cover: z.string(),
+        published: z.boolean().optional().default(false),
+        date: z.any(),
+        type: z.string().optional(),
+    }),
 });
 
 export const collections = {
-  posts: typCollection,
+    posts: typCollection,
 };
